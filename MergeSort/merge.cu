b11e4b36d20e0205c04247f006bb993f4b3f8e07
sbolla2 <67021449+sbolla2@users.noreply.github.com>
2023-04-21 17:48:42 -0400

    Add files via upload

---
+++ MergeSort/merge.cu
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+__global__ void merge(int* arraylist, int* temparray, int left, int right)
+{
+    //variable
+    int mid = (left + right)/2;
+    int length = right - left;
+    //stream for right array and left array
+    cudaStream_t startleft, startright;
+    
+    //start to return if the array is 1
+    if(length < 2){
+        return;
+    }
+    //create block for the left array
+    cudaStreamCreateWithFlags(&startleft, cudaStreamNonBlocking);
+    merge<<<1,1,0, startleft>>>(arraylist, temparray, left, mid);
+    cudaStreamDestroy(startleft);
+    
+    //create block for the right array
+    cudaStreamCreateWithFlags(&startright, cudaStreamNonBlocking);
+    merge<<<1,1,0, startright>>>(arraylist, temparray, mid, right);
+    cudaStreamDestroy(startright);
+
+    cudaDeviceSynchronize();
+    int i;
+    int templeft = left;
+    int tempmid = mid;
+    for(i = left; i < right; i++){
+        if(templeft < mid && (tempmid >= right || arraylist[templeft] <= arraylist[tempmid])){
+            temparray[i] = arraylist[templeft];
+            templeft++;
+        }
+        else{
+            temparray[i] = arraylist[tempmid];
+            tempmid++;
+        }
+    }
+    for(i = left; i < right; i++){
+        arraylist[i] = temparray[i];
+    }
+}
+
+extern "C" void sort(int* array, int num){
+    int* gpuarray;
+    int* temparray;
+    int leftnum = 0;
+    int rightnum = num;
+
+    int arraysize = num * sizeof(int);
+    //allocating memory for the array
+    cudaMalloc((void**)&gpuarray, arraysize);
+    cudaMalloc((void**)&temparray, arraysize);
+    cudaMemcpy(gpuarray, array, arraysize, cudaMemcpyHostToDevice);
+    //call the merge function to start sorting
+    merge<<<1, 1>>>(gpuarray, temparray, leftnum, rightnum);
+    cudaDeviceSynchronize();
+    //copy the array in the sorting function back to the main array
+        //free the array    
+    cudaMemcpy(array, gpuarray, arraysize, cudaMemcpyDeviceToHost);
+    cudaFree(temparray);
+    cudaFree(gpuarray);
+    cudaDeviceReset();
+}
+
+
+
+extern "C" void initialize_CUDA(int rank){
+    int cudaDeviceCount;
+    int cE = cudaGetDeviceCount( &cudaDeviceCount);
+    if( cE != cudaSuccess )
+    {
+        printf(" Unable to determine cuda device count, error is %d, count is %d\n",
+            cE, cudaDeviceCount );
+        exit(-1);
+    }
+    cE = cudaGetDeviceCount( &cudaDeviceCount);
+    if( cE != cudaSuccess )
+    {
+        printf(" Unable to have rank %d set to cuda device %d, error is %d \n",
+            rank, (rank % cudaDeviceCount), cE);
+        exit(-1);
+    }
+}
