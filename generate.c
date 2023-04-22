3c265747e2a8f739b7a5c2d9fe9535a3d814760a
VQTran123 <34687515+VQTran123@users.noreply.github.com>
2023-04-22 16:18:02 -0400

    Fixed stuff

--- generate.c
+++ generate.c
@@ -1,7 +1,9 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <ctime>
 int main(){
-	char *filename = "random.txt";
+    srand(time(NULL));
+	char *filename = "random.bin";
     long x;
 	FILE *fp = fopen(filename, "wb");
 	if (fp == NULL){
@@ -9,9 +11,17 @@ int main(){
 		return -1;
 	}
 	for(int i = 0; i < 1048576; i++){
-        x = (long) rand();
+        x = rand();
 		fwrite (&x, sizeof (x), 1, fp);
 	}
-	fclose(fp);
+    fclose(fp);
+    FILE * fh = fopen ("random.bin", "rb");
+    for(int i = 0; i < 1048576; i++){
+        if (fh != NULL) {
+            fread (&x, sizeof (x), 1, fh);
+            fclose (fh);
+        }
+        printf ("Value is: %d\n", x);
+    }
 	return 0;
 }
