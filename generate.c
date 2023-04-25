d4282ceb93dbe02e24c5042b6f465c44f805c92f
sbolla2 <67021449+sbolla2@users.noreply.github.com>
2023-04-24 21:01:56 -0400

    Add files via upload

---
+++ generate.c
@@ -0,0 +1,23 @@
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    time_t t;
+    srand((unsigned) time(&t));
+	char *filename = "random.bin";
+    long x;
+    
+	FILE *fp = fopen(filename, "wb");
+	if (fp == NULL){
+		printf("Error opening the file %s", filename);
+		return -1;
+	}
+	for(int i = 0; i < 1048576; i++){
+        x = rand() % 1048576;
+        printf ("Value is: %ld\n", x);
+		fwrite (&x, sizeof (x), 1, fp);
+	}
+    fclose(fp);
+	return 0;
+}
